402c7a4fd7bc30e94e511ab2696f1bd14f371b66
Amir1453 <63464110+Amir1453@users.noreply.github.com>
2022-06-08 23:39:23 +0300

    added sanitization

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -3,6 +3,27 @@ from pathlib import Path
 from mutagen.mp3 import MP3
 from utils.console import print_step, print_substep
 from rich.progress import track
+import re
+
+
+def sanitize_text(reddit_obj):
+    """
+    Sanitizes the text for tts.
+    What gets removed:
+    - following characters`^_~@!&;#:-%“”‘"%*/{}[]()\|<>?=+`
+    - any http or https links
+    """
+
+    # remove any urls from the text
+    regex_urls = r"((http|https)://[^\s]+)"
+    result = re.sub(regex_urls, " ", reddit_obj)
+
+    # note: not removing apostrophes
+    regex_expr = r"\s['|’]|['|’]\s|[\^_~@!&;#:\-%“”‘\"%\*/{}\[\]\(\)\\|<>=+]"
+    result = re.sub(regex_expr, " ", result)
+
+    # remove extra whitespace
+    return " ".join(result.split())
 
 
 def save_text_to_mp3(reddit_obj):
@@ -17,7 +38,8 @@ def save_text_to_mp3(reddit_obj):
     # Create a folder for the mp3 files.
     Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False)
+    thread_title = sanitize_text(reddit_obj["thread_title"])
+    tts = gTTS(text=thread_title, lang="en", slow=False)
     tts.save(f"assets/mp3/title.mp3")
     length += MP3(f"assets/mp3/title.mp3").info.length
 
@@ -27,7 +49,8 @@ def save_text_to_mp3(reddit_obj):
         pass
 
     if reddit_obj["thread_post"] != "":
-        tts = gTTS(text=reddit_obj["thread_post"], lang="en", slow=False)
+        thread_post = sanitize_text(reddit_obj["thread_post"])
+        tts = gTTS(text=thread_post, lang="en", slow=False)
         tts.save(f"assets/mp3/posttext.mp3")
         length += MP3(f"assets/mp3/posttext.mp3").info.length
 
@@ -35,7 +58,8 @@ def save_text_to_mp3(reddit_obj):
         # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
         if length > 50:
             break
-        tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
+        comment = sanitize_text(comment["comment_body"])
+        tts = gTTS(text=comment, lang="en", slow=False)
         tts.save(f"assets/mp3/{idx}.mp3")
         length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
